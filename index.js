17b4ce9fc55cf15bb28775f0b575418357da1c6c
cardavinci1 <cardavinci1@gmail.com>
2019-05-28 14:48:35 -0500

    changed greeting

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const app = express();
 
 require("./routes/authRoutes")(app);
 
+app.get("/", (req, res) => {
+  res.send({ hi: "there" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
