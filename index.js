4514217a50e95c8ec03dc893fdc84f45c6b872da
cardavinci1 <cardavinci1@gmail.com>
2019-05-28 14:57:45 -0500

    changed greeting1

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-require("./services/passport");
+// require("./services/passport");
 
 const app = express();
 
-require("./routes/authRoutes")(app);
+// require("./routes/authRoutes")(app);
 
 app.get("/", (req, res) => {
   res.send({ hi: "there" });
